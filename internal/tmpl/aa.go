f5818210f1a76887562885fe54dcc68521a93651
xugo <xx@golang.space>
2024-10-21 17:00:54 +0800

    移除依赖包，降低 go 版本

--- internal/tmpl/aa.go
+++ internal/tmpl/aa.go
@@ -1,7 +1,5 @@
 package tmpl
 
-import "github.com/ixugo/goweb/pkg/orm"
-
 type User struct {
 	Age      int64  //  年龄
 	Name     string // 昵称
@@ -9,9 +7,7 @@ type User struct {
 }
 
 type UserLogs struct {
-	ID        int
-	CreatedAt orm.Time // 创建时间
-	UpdatedAt orm.Time // 更新时间
-	Action    string   // 行为
-	IP        string   // 操作 ip
+	ID     int
+	Action string // 行为
+	IP     string // 操作 ip
 }
